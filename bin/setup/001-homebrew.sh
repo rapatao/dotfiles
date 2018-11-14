7d042bad873494ef666f5efb1a646cbaa2431c4f
Luiz H. Rapatao <luiz@rapatao.com>
2018-11-13 22:28:26 -0200

    configure brew only in macos environment

--- bin/setup/001-homebrew.sh
+++ bin/setup/001-homebrew.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-brew update
-brew upgrade
-brew cask upgrade
+if [[ $(uname -s) == 'Darwin' ]]; then
+    brew update
+    brew upgrade
+    brew cask upgrade
 
-brew install coreutils moreutils findutils bash zsh
-brew install gnu-sed --with-default-names
-brew install wget --with-iri
-brew install vim --override-system-vi
+    brew install coreutils moreutils findutils bash zsh
+    brew install gnu-sed --with-default-names
+    brew install wget --with-iri
+    brew install vim --override-system-vi
 
-brew cleanup
+    brew cleanup
+fi;
