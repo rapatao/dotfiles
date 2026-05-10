594fe59e5c7b7daf691229daa1aabc43a4375108
Luiz Henrique Rapatao <rapatao@rapatao.com>
2026-05-10 20:12:40 +0100

    feat: add security and web tools configurations
    # Conflicts:
    #	apps/_security.nix
    #	apps/_web.nix

--- apps/_security.nix
+++ apps/_security.nix
@@ -1,6 +1,4 @@
 { lib, config, pkgs, ... }: {
-  options.apps.security = lib.mkEnableOption "Install security tools";
-
   config = lib.mkIf config.apps.security {
     environment = {
       systemPackages = [
--- apps/_web.nix
+++ apps/_web.nix
@@ -1,6 +1,4 @@
 { lib, config, pkgs, ... }: {
-  options.apps.web = lib.mkEnableOption "Install web tools";
-
   config = lib.mkIf config.apps.web {
     homebrew = {
       casks = [
